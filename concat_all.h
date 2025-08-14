0b7f89ac571ba5078d8f437c2feb50fcf38da8a1
Edgardo Carreras <me@edgardocarreras.com>
2025-08-14 01:43:59 -0400

    Add Node creations with test

--- concat_all.h
+++ concat_all.h
@@ -1,11 +1,31 @@
 #pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 typedef enum {
     Integer, String, List, Nil
    } NodeType;
 
-char *ConcatAll(NodeType *list) {
-    char* answer =  "";
-    return answer;
-
+static char *ConcatAll(const NodeType *list) {
+    switch (*list) {
+        case Integer:
+        case String:
+            return strdup((char *)(list + 1));
+    }
+    return strdup("");
 }
+
+static NodeType * StringNodeNew(const char * str) {
+    const size_t len = strlen(str) + 1;
+    auto * stringNode = (NodeType *)malloc(sizeof(NodeType) + len);
+    if (!stringNode) return NULL;
+    stringNode[0] = String;
+    memcpy((char *)(stringNode+1), str, len);
+    return stringNode;
+}
+
+#ifdef __cplusplus
+}
+#endif
--- concat_all_tests.cc
+++ concat_all_tests.cc
@@ -2,35 +2,64 @@
 #include <gtest/gtest.h>
 #include "concat_all.h"
 
-TEST(ConcatAll, ListOfString) {
-    NodeType * heapString1 = (NodeType*)strdup("Frodo");
-    NodeType * heapString2 = (NodeType*)strdup("Baggins");
-    NodeType list[3];
-    list[0] = String;
-    list[1] = *heapString1;
-    list[2] = *heapString2;
-    char *answer = ConcatAll(list);
-    const char* expected = "FrodoBaggins";
-    EXPECT_STREQ(answer, expected);
-    free(heapString1);
-    free(heapString2);
-
-
-}
-TEST(ConcatAll, IgnoresIntegers) {
-    NodeType * memInt = (NodeType*)malloc(sizeof(int));
-    NodeType list[2];
-    list[0] = Integer;
-    list[1] = *memInt;
-    char *answer = ConcatAll(list);
-    const char* expected = "";
-    EXPECT_STREQ(answer, expected);
-    free(memInt);
-}
-
 TEST(ConcatAll, Empty) {
     NodeType list[0];
-    char *answer = ConcatAll(list);
+    const char *answer = ConcatAll(list);
     const char* expected = "";
     EXPECT_STREQ(answer, expected);
 }
+
+TEST(ConcatAll, IgnoresIntegers) {
+    auto * intNode = (NodeType*)malloc(sizeof(NodeType) + sizeof(int));
+    intNode[0] = Integer;
+    const char *answer = ConcatAll(intNode);
+    const char* expected = "";
+    EXPECT_STREQ(answer, expected);
+    free(intNode);
+}
+
+
+
+TEST(ConcatAll, OneString) {
+    auto stringNode = StringNodeNew("Frodo");
+    const char *answer = ConcatAll(stringNode);
+    const char* expected = "Frodo";
+    EXPECT_STREQ(answer, expected);
+    free(stringNode);
+}
+
+// TEST(ConcatAll, ListMix) {
+//     auto heapString1 = (NodeType *)(strdup("Frodo"));
+//     NodeType list[2];
+//     list[0] = String;
+//     list[1] = *heapString1;
+//     const char *answer = ConcatAll(list);
+//     const char* expected = "Frodo";
+//     EXPECT_STREQ(answer, expected);
+//     free(heapString1);
+// }
+
+
+TEST(NodeTypeTest, StringNew) {
+    const NodeType * stringNode = StringNodeNew("Frodo");
+    EXPECT_EQ(stringNode[0], String);
+    const char * expected = "Frodo";
+    EXPECT_STREQ((char *)(stringNode+ 1), expected);
+}
+
+// NodeType * ListNodeNew(const NodeType* head,  const NodeType* tail) {
+//     NodeType * listNode = (NodeType *)malloc(sizeof(NodeType) + sizeof(NodeType *) * 2);
+//     listNode[0] = List;
+//     listNode[1] = *head;
+//     listNode[2] = *tail;
+//     return listNode;
+// }
+// TEST(NodeTypeTest, ListStrings) {
+//     const NodeType * stringNode = StringNodeNew("Frodo");
+//     const NodeType * stringNode2 = StringNodeNew("Baggins");
+//     const NodeType * listStringNode = ListNodeNew(stringNode, stringNode2);
+//     EXPECT_EQ(listStringNode[0], List);
+//     NodeType** pair = (NodeType**)listStringNode[1];
+//     EXPECT_STREQ((char *)pair[0], strdup("Frodo"));
+//     EXPECT_STREQ((char *)pair[1], strdup("Baggins"));
+// }
